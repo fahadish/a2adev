f6eb315cd0221fdaca12581f0fb2b1825dfa1e61
fahadish <fahadishfaq45@gmail.com>
2023-08-22 00:25:46 +0500

    for adeel

--- lib/main.dart
+++ lib/main.dart
@@ -5,31 +5,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'controller/authController.dart';
-//
-// Future main() async{
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   Get.put(AuthController());
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ScreenUtilInit(
-//       designSize: const Size(360, 690),
-//       minTextAdapt: true,
-//       splitScreenMode: true,
-//       builder: (context, child) {
-//         return const GetMaterialApp(
-//           home: SplashScreen(),
-//         );
-//       },
-//     );
-//   }
-// }
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
